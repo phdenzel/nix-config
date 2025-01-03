39a169c9bb112f9d62c554cfcddfea52addee536
phdenzel <phdenzel@gmail.com>
2025-01-03 14:01:14 +0100

    Separated options into components

--- modules/amd-drivers.nix
+++ modules/amd-drivers.nix
@@ -14,52 +14,61 @@ in {
       type = types.bool;
       default = false;
     };
-    useVlk = mkOption {
+    vlk.enable = mkOption {
       description = "Use AMD's open source Vulkan driver.";
       type = types.bool;
       default = true;
     };
-    installUtils = mkOption {
+    utils.install = mkOption {
       description = "Install AMD graphics utilities.";
       type = types.bool;
       default = true;
     };
-  };
-
-  config = mkIf cfg.enable {
-    # Load driver into kernel
-    hardware.amdgpu.initrd.enable = mkDefault true;
-    # Load driver for Xorg and wayland
-    services.xserver.videoDrivers = mkDefault ["amdgpu"];
-    # HIP libraries
-    systemd.tmpfiles.rules = with pkgs;
-      mkDefault [
-        "L+    /opt/rocm/hip   -    -    -     -    ${rocmPackages.clr}"
-      ];
-    # Mesa drivers
-    hardware.graphics.enable = mkDefault true;
-    hardware.graphics.enable32Bit = mkDefault true;
-    # Vulkan
-    hardware.amdgpu.amdvlk.enable = mkDefault cfg.useVlk;
-    hardware.amdgpu.amdvlk.support32Bit.enable = mkDefault cfg.useVlk;
-    hardware.amdgpu.amdvlk.supportExperimental.enable = mkDefault cfg.useVlk && cfg.experimentalSupport;
-    # hardware.amdgpu.amdvlk.settings = {
-    #   AllowVkPipelineCachingToDisk = 1; # ~/.cache/AMD/VkCache
-    #   ShaderCacheMode = 1;
-    #   IFH = 0;
-    #   EnableVmAlwaysValid = 1;
-    #   IdleAfterSubmitGpuMask = 1;
-    # };
-    # environment.variables.AMD_VULKAN_ICD = mkIf forceVlk "RADV";
-    # GPU utils
-    environment.systemPackages = with pkgs;
-      mkIf cfg.installUtils
-      [
-        btop-rocm
+    utils.packages = mkOption {
+      description = "List of utility packages.";
+      type = with types; listOf package;
+      default = with pkgs; [
         rocmPackages.rocm-smi
+        btop-rocm
         clinfo
         libva-utils
         vdpauinfo
       ];
+    };
   };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      # Load driver into kernel
+      hardware.amdgpu.initrd.enable = mkDefault true;
+      # Load driver for Xorg and wayland
+      # services.xserver.videoDrivers = mkDefault ["amdgpu"];
+      services.xserver.videoDrivers = mkDefault ["modesetting"];
+      # Mesa drivers
+      hardware.graphics.enable = mkDefault true;
+      hardware.graphics.enable32Bit = mkDefault true;
+      # HIP libraries
+      systemd.tmpfiles.rules = with pkgs;
+        mkDefault [
+          "L+    /opt/rocm/hip   -    -    -     -    ${rocmPackages.clr}"
+        ];
+    })
+    (mkIf cfg.vlk.enable {
+      # Vulkan
+      hardware.amdgpu.amdvlk.enable = mkDefault cfg.useVlk;
+      hardware.amdgpu.amdvlk.support32Bit.enable = mkDefault cfg.useVlk;
+      hardware.amdgpu.amdvlk.supportExperimental.enable = mkDefault cfg.useVlk && cfg.experimentalSupport;
+      # hardware.amdgpu.amdvlk.settings = {
+      #   AllowVkPipelineCachingToDisk = 1; # ~/.cache/AMD/VkCache
+      #   ShaderCacheMode = 1;
+      #   IFH = 0;
+      #   EnableVmAlwaysValid = 1;
+      #   IdleAfterSubmitGpuMask = 1;
+      # };
+      # environment.variables.AMD_VULKAN_ICD = mkIf forceVlk "RADV";
+    })
+    (mkIf cfg.utils.install {
+      environment.systemPackages = cfg.utils.packages;
+    })
+  ];
 }
