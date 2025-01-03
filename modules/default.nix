4d3fb5cf2862d1eab25b5905f4ac7c34b43ce13f
phdenzel <phdenzel@gmail.com>
2025-01-03 17:14:25 +0100

    Fixed typo in nvidia prime drivers import

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,6 @@
 {
   drivers.amdgpu = import ./amd-drivers.nix;
   drivers.nvidia = import ./nvidia-drivers.nix;
-  drivers.nvidia-prime = import ./nvidia-primedrivers.nix;
+  drivers.nvidia-prime = import ./nvidia-prime-drivers.nix;
   intl = import ./intl.nix;
 }
