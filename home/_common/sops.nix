8f31032ec341464ea18e6c67b6e7fdcf5736d38a
phdenzel <phdenzel@gmail.com>
2025-01-05 14:32:18 +0100

    Try to fix rebuild error: failed unmount /var

--- home/_common/sops.nix
+++ home/_common/sops.nix
@@ -18,8 +18,8 @@ in {
 
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      keyFile = "/var/lib/sops-nix/key.txt";
-      generateKey = true;
+      # keyFile = "/var/lib/sops-nix/key.txt";
+      # generateKey = true;
     };
 
     secrets = {
