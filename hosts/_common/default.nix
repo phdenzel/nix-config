9ab630fb350105fa4f47e8201104760a4b2ab7e9
phdenzel <phdenzel@gmail.com>
2025-01-05 23:03:20 +0100

    Moved nix common config out of default file

--- hosts/_common/default.nix
+++ hosts/_common/default.nix
@@ -1,44 +1,7 @@
 # Common configuration for all hosts
-{
-  pkgs,
-  lib,
-  inputs,
-  outputs,
-  ...
-}: let
-  flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-in {
+{...}: {
   imports = [
+    ./nix.nix
     ./sops.nix
   ];
-
-  nix = {
-    settings = {
-      experimental-features = "nix-command flakes";
-      trusted-users = ["root" "@wheel" "phdenzel"];
-      auto-optimise-store = lib.mkDefault true;
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than +5";
-    };
-    optimise.automatic = true;
-
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
-
-  hardware.enableRedistributableFirmware = true;
-
-  nixpkgs = {
-    overlays = builtins.attrValues outputs.overlays;
-    config = {
-      allowUnfree = true;
-    };
-  };
-
-  # User settings
-  users.mutableUsers = false;
-  users.defaultUserShell = pkgs.bash;
 }
