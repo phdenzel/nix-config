940c8775536258b72e2e8c36ccec7d36a21744fd
phdenzel <phdenzel@gmail.com>
2025-01-05 13:08:10 +0100

    Change to host key usage only

--- home/_common/sops.nix
+++ home/_common/sops.nix
@@ -16,7 +16,11 @@ in {
     defaultSopsFile = builtins.toPath "${secretsFile}";
     validateSopsFiles = false;
 
-    age.keyFile = "/home/${sopsUser}/.config/sops/age/keys.txt";
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
+    };
 
     secrets = {
       "passwd/${sopsUser}" = {
