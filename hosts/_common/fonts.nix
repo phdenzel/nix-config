c39bcfdef5d2463db1951688d7eb1a99f2acc3a4
phdenzel <phdenzel@gmail.com>
2025-01-10 17:54:47 +0100

    Fixed and formatted fonts packages

--- hosts/_common/fonts.nix
+++ hosts/_common/fonts.nix
@@ -1,12 +1,13 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    dejavu_fonts
-    hubot-sans
-    mona-sans
-    nerdfonts
-    noto-fonts
-    open-sans
-    roboto
-    ubuntu-sans
-  ];
+  fonts.packages = with pkgs;
+    [
+      dejavu_fonts
+      hubot-sans
+      mona-sans
+      noto-fonts
+      open-sans
+      roboto
+      ubuntu-sans
+    ]
+    ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
 }
