fd2dc2d189c48f4a8288e41f389bfea2a83dc983
phdenzel <phdenzel@gmail.com>
2025-01-12 15:11:08 +0100

    Fixed typo

--- hosts/_common/stylix.nix
+++ hosts/_common/stylix.nix
@@ -1,7 +1,7 @@
 {inputs, lib, ...}: with lib; {
   imports = [inputs.stylix.nixosModules.stylix];
   # Stylix host defaults (required settings only)
-  stylix.enable = mkDefault rue;
+  stylix.enable = mkDefault true;
   stylix.image = mkDefault ../../home/_common/stylix/nixos-default-wallpaper.png;
   stylix.polarity = mkDefault "dark";
 }
