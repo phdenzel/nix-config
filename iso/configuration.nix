1e7b8786567fcbee50df52058ea4fb6b2a514bfe
phdenzel <phdenzel@gmail.com>
2025-01-13 09:40:59 +0100

    Switched iso config to grub bootloader

--- iso/configuration.nix
+++ iso/configuration.nix
@@ -8,8 +8,13 @@
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = false;
+    grub.enable = true;
+    grub.efiSupport = true;
+    grub.devices = ["nodev"];
+    efi.canTouchEfiVariables = true;
+  };
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
