c512fa590b07e3b265b9900c29f1809e1816fd15
phdenzel <phdenzel@gmail.com>
2025-02-10 10:19:07 +0100

    Moved jupterhub to python3.12

--- hosts/_common/computing.nix
+++ hosts/_common/computing.nix
@@ -1,6 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   services = {
     jupyterhub.enable = true;
+    jupyterhub.jupyterhubEnv = pkgs.python312.withPackages (p: with p; [
+      jupyterhub
+      jupyterhub-systemdspawner
+    ]);
+    jupyterhub.jupyterlabEnv = pkgs.python312.withPackages (p: with p; [
+      jupyterhub
+      jupyterlab
+    ]);
     jupyterhub.port = 8000;
   };
 }
