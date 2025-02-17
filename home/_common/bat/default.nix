7731ebf39cd083450687d8f294f55cdc6250e30a
phdenzel <phdenzel@gmail.com>
2025-02-17 16:15:51 +0100

    Custom theme for bat

--- home/_common/bat/default.nix
+++ home/_common/bat/default.nix
@@ -1,5 +1,10 @@
-{...}: {
+{config, lib, ...}: {
+  imports = [./theme.nix];
+  
   programs.bat = {
     enable = true;
+    config = {
+      theme = lib.mkOverride 99 "${config.colorScheme.slug}";
+    };
   };
 }
