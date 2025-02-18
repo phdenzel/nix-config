8702c5257281db10cab6bb7d200c34c04a24717d
phdenzel <phdenzel@gmail.com>
2025-02-18 23:48:32 +0100

    Configured starship

--- home/_common/starship/default.nix
+++ home/_common/starship/default.nix
@@ -1,10 +1,48 @@
-{pkgs, ...}: {
-  programs.starship = {
+{pkgs, config, ...}: {
+  imports = [./nerd-fonts.nix ./disabled.nix];
+  
+  programs.starship = with config.colorScheme.palette; {
     enable = true;
     package = pkgs.starship;
-    # settings = {
-    #   add_newline = true;
-    #   format = ""
-    # };
+    settings = {
+      add_newline = true;
+      format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$git_branch$git_commit$git_state$git_metrics$git_status$docker_context$package$c$cmake$dotnet$elixir$elm$erlang$golang$haskell$helm$java$julia$gradle$lua$nodejs$perl$php$python$ruby$rust$scala$solidity$terraform$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$direnv$env_var$custom$line_break$jobs$time$status$os$container$shell$character";
+      right_format = "$cmd_duration$sudo";
+      character = {
+        # success_symbol = "[](blue)[](bold green)";
+        success_symbol = "[](blue) [❯](bold green)";
+        error_symbol = "[](blue) [❯](bold red)";
+      };
+      username = {
+        format = "[$user](bold purple)";
+      };
+      hostname = {
+        style = "bold blue";
+        format = "[$ssh_symbol](bold bright-yellow)[$hostname]($style):";
+      };
+      git_branch = {
+        style = "bold bright-yellow";
+        format = "[$symbol$branch(:$remote_branch)]($style) ";
+      };
+      git_status = {
+        style = "bold #${purple}";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+      };
+      sudo = {
+        disabled = false;
+        symbol = " ";
+        style = "bold purple";
+        format = "[$symbol]($style)";
+      };
+      python = {
+        style = "bold blue";
+        format = "[\${symbol}\${pyenv_prefix}(\${version} )(\($virtualenv\) )]($style)";
+      };
+      nix_shell = {
+        format = "[$symbol$state( \($name\))]($style) ";
+      };
+    };
   };
 }
---
+++ home/_common/starship/disabled.nix
@@ -0,0 +1,33 @@
+{...}: {
+  programs.starship.settings = {
+    vcsh.disabled = true;
+    fossil_branch.disabled = true;
+    fossil_metrics.disabled = true;
+    hg_branch.disabled = true;
+    pijul_channel.disabled = true;
+    cobol.disabled = true;
+    daml.disabled = true;
+    dart.disabled = true;
+    deno.disabled = true;
+    elm.disabled = true;
+    fennel.disabled = true;
+    gleam.disabled = true;
+    guix_shell.disabled = true;
+    haxe.disabled = true;
+    kotlin.disabled = true;
+    nim.disabled = true;
+    opa.disabled = true;
+    pulumi.disabled = true;
+    purescript.disabled = true;
+    quarto.disabled = true;
+    raku.disabled = true;
+    rlang.disabled = true;
+    red.disabled = true;
+    swift.disabled = true;
+    typst.disabled = true;
+    vlang.disabled = true;
+    nats.disabled = true;
+    crystal.disabled = true;
+    battery.disabled = true;
+  };
+}
