beb3e5d04d80123a6c493054ac01df209e7b5470
phdenzel <phdenzel@gmail.com>
2025-02-21 15:37:58 +0100

    Limit browserpass for firefox and brave

--- home/_common/firefox/default.nix
+++ home/_common/firefox/default.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
   imports = [./profile.nix];
 
-  programs.browserpass.enable = true;
+  programs.browserpass = {
+    enable = true;
+    browsers = ["firefox" "brave"];
+  };
   programs.firefox = {
     enable = true;
     package = pkgs.firefox;
