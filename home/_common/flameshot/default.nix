6ad88450d4509a1e5ceb6505ebac4c327c7e7360
phdenzel <phdenzel@gmail.com>
2025-03-30 18:44:22 +0200

    Fixed flameshot override for hm service

--- home/_common/flameshot/default.nix
+++ home/_common/flameshot/default.nix
@@ -1,6 +1,7 @@
-{...}: {
+{pkgs, ...}: {
   services.flameshot = {
     enable = true;
+    package = (pkgs.flameshot.override {enableWlrSupport = true;});
     settings = {
       General = {
         disabledTrayIcon = false;
