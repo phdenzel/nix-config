7fc45b4f858384dd7d3a8435682a6148ac60a604
phdenzel <phdenzel@gmail.com>
2025-05-22 07:19:19 +0200

    Added cscs-keygen application

--- home/phdenzel/_configs/applications/cscs-keygen.nix
+++ home/phdenzel/_configs/applications/cscs-keygen.nix
@@ -1,21 +1,19 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    cscs-keygen
-  ];
-  nixpkgs.config.packageOverrides = pkgs: {
-    cscs-keygen =
-      let
-        default = builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/phdenzel/cscs-sshservice-cli/refs/heads/main/default.nix";
-          sha256 = "";
-        };
-      in pkgs.callPackage default {
-        src = pkgs.fetchFromGitHub {
-          owner = "phdenzel";
-          repo = "cscs-sshservice-cli";
-          rev = "main";
-          sha256 = "";
-        };
+  home.packages = let
+    default = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/phdenzel/cscs-sshservice-cli/refs/heads/main/default.nix";
+      sha256 = "sha256:19jsp79n5w2nrpf805kgq44qcfwfbwn3jg6srpfa36w8ljrsqcni";
+    };
+    cscs-keygen = pkgs.callPackage default {
+      src = pkgs.fetchFromGitHub {
+        owner = "phdenzel";
+        repo = "cscs-sshservice-cli";
+        rev = "main";
+        sha256 = "sha256-GSVj2KNBLeALhi/YZzJVylB19TQD32ihFbj9oO8Y+r4=";
       };
-  };
+    };
+  in
+    [
+      cscs-keygen
+    ];
 }
