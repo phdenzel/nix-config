1af798854e68e7bbe8fcc24001683c4fb56ec2d6
phdenzel <phdenzel@gmail.com>
2025-06-14 17:38:44 +0200

    Syncthing init new machines

--- home/_common/syncthing/_devices.nix
+++ home/_common/syncthing/_devices.nix
@@ -3,16 +3,22 @@
     "phinix" = {
       id = "Q7GMTVX-CSOMIDN-YPW3QO7-S2FWTJD-RGHDRNO-6Z7NZBH-WHEQ4KB-RO6OXA3";
     };
+    "sol" = {
+      # id = "";
+    };
     "fenrix" = {
       id = "LAX3AZA-OADG4U7-SP3FWXD-YYBIKKS-DHNY3IF-6FIXGYU-VY6W2BI-O773RQD";
+    };
+    "ygdrasil" = {
+      # id = "";
     };
     "asahi" = {
       id = "TEK3CC3-2JU5QE2-S55OP5E-PSCFF7K-IMLVPAA-ZBJWE25-4CTNUDB-J7IPIQI";
     };  
   };
   groups = {
-    all = ["phinix" "fenrix" "asahi"];
-    home = ["phinix" "asahi"];
+    all = ["phinix" "fenrix" "sol" "ygdrasil" "asahi"];
+    home = ["phinix" "sol" "ygdrasil" "asahi"];
     work = ["fenrix"];
   };
 }
