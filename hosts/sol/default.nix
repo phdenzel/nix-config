11c431f72026fffee2e8b423c1669c5f95bb3477
phdenzel <phdenzel@gmail.com>
2025-06-16 22:49:14 +0200

    Host sol: hardware configuration

--- hosts/sol/default.nix
+++ hosts/sol/default.nix
@@ -2,7 +2,7 @@
   imports = [
     inputs.disko.nixosModules.disko
     ./disk-config.nix
-    # ./hardware-configuration.nix
+    ./hardware-configuration.nix
     ./configuration.nix
     ./users.nix
   ];
