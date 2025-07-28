19542a477ae0a5f6655231d539d6076b5eee6225
phdenzel <phdenzel@gmail.com>
2025-07-28 11:50:39 +0200

    Updated pyverto url sha256

--- home/phdenzel/_configs/applications/pyverto.nix
+++ home/phdenzel/_configs/applications/pyverto.nix
@@ -2,7 +2,7 @@
   home.packages = let
     default = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/phdenzel/pyverto/refs/heads/main/default.nix";
-      sha256 = "";
+      sha256 = "sha256:1m5xzxrpy5vg6ffxvvjwk8db9a9ljm0yg5awmvwqkffq3286sz9q";
     };
     pyverto = pkgs.callPackage default {
       src = pkgs.fetchFromGitHub {
