01e249daa8f3cd08964c06aa6008fa9567872097
phdenzel <phdenzel@gmail.com>
2025-12-08 00:08:24 +0100

    Misc AI packages

---
+++ hosts/_common/ai.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+with lib; {
+  environment.systemPackages = with pkgs; [
+    claude-code
+    librechat
+    # whisperx
+  ];
+
+  services = {
+    n8n = {
+      enable = true;
+      openFirewall = true;
+      environment = {
+        N8N_PORT = 5678;
+        N8N_DIAGNOSTICS_ENABLED = false;  # disables Ask AI in the code node
+        N8N_VERSION_NOTIFICATIONS_ENABLED = false;
+      };
+    };
+  };
+}
