e4a9d26df491dcbb88aca1571e63f27d634b4316
phdenzel <phdenzel@gmail.com>
2025-12-27 08:40:52 +0100

    Setup binary cache via cachix

--- hosts/_common/nix.nix
+++ hosts/_common/nix.nix
@@ -14,11 +14,24 @@ in {
       trusted-users = ["root" "@wheel" "phdenzel"];
       auto-optimise-store = lib.mkDefault true;
       download-buffer-size = 524288000;
+
+      # binary caches
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 5d";
+      persistent = true;
     };
     optimise.automatic = true;
 
