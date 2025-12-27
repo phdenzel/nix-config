1e63e5036b7b276bdf259bfa178b4fb97ac7d44f
phdenzel <phdenzel@gmail.com>
2025-12-27 09:18:02 +0100

    Add yazi plugins

--- home/_common/yazi/default.nix
+++ home/_common/yazi/default.nix
@@ -1,5 +1,44 @@
-{...}: {
+{pkgs, ...}: {
   programs.yazi = {
     enable = true;
+    enableZshIntegration = true;
+    shellWrapperName = "y";  # type `yy` in shell to exit in cwd
+
+    plugins = with pkgs.yaziPlugins; {
+      chmod = chmod;
+      compress = compress;
+      git = git;
+      starship = starship;
+    };
+
+    initLua = ''
+			require("starship"):setup()
+      require("git"):setup()
+		'';
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = ["c" "m"];
+          run = "plugin chmod";
+          desc = "Chmod on selected files";
+        }
+        {
+          on = ["c" "a" "a"];
+          run = "plugin compress";
+          desc = "Archive selected files";
+        }
+        {
+          on = ["c" "a" "l"];
+          run = "plugin compress -l";
+          desc = "Archive selected files (compression level)";
+        }
+        {
+          on = ["c" "a" "t"];
+          run = "plugin compress -l tar.gz";
+          desc = "Archive selected files (tarball)";
+        }
+      ];
+    };
   };
 }
