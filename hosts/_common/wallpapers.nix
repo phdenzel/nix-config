b4dc57138d0c8eaca09c026aa147b8cb38289a27
phdenzel <phdenzel@gmail.com>
2026-03-14 12:25:43 +0100

    fix: wallpaper stylix references as strings

--- hosts/_common/wallpapers.nix
+++ hosts/_common/wallpapers.nix
@@ -4,12 +4,6 @@
     ln -s ${inputs.phd-wallpapers} $out/share/backgrounds/nixos
   '';
 in {
-  # TODO: fix by adding phd-wallpapers to environment.systemPackages and environment.pathsToLink = ["/share/backgrounds/nixos"] https://discourse.nixos.org/t/is-there-a-permanent-folder-for-official-wallpapers/58945
   environment.systemPackages = [ phd-wallpapers ];
-  environment.pathsToLink = [ "share/backgrounds" ];
-  environment.etc = {
-    wallpapers = {
-      source = inputs.phd-wallpapers;
-    };
-  };
+  environment.pathsToLink = [ "/share/backgrounds" ];
 }
