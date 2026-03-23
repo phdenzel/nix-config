673285302c0e567adb991451b53df86596ab04f9
phdenzel <phdenzel@gmail.com>
2026-03-23 19:51:48 +0100

    fix: change hashes due to cscs-keygen-cli update

--- home/phdenzel/_configs/applications/cscs-keygen.nix
+++ home/phdenzel/_configs/applications/cscs-keygen.nix
@@ -2,14 +2,14 @@
   home.packages = let
     default = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/phdenzel/cscs-sshservice-cli/refs/heads/main/default.nix";
-      sha256 = "sha256:0mqwn1lb1770ph1ls3l8axzrnhc1d11bldr32423ffdf7fww5wyd";
+      sha256 = "sha256:1lakwmn80ab2m3c43z1rwj4v5gk2a18hfma7ik9n2kqx1zrn75bq";
     };
     cscs-keygen = pkgs.callPackage default {
       src = pkgs.fetchFromGitHub {
         owner = "phdenzel";
         repo = "cscs-sshservice-cli";
         rev = "main";
-        sha256 = "sha256-M3wOxYDj841mlZBI0b3+0udGDdlZhHo1Mec8vqBWsao=";
+        sha256 = "sha256-gbQh646rT/acFvsdxqqFwAuh3qyUvRylnjh8GbahTp4=";
       };
     };
   in
