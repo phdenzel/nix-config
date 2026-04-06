9f086ecf0ddb3fe6b5fa497de0763fd89303a3b8
phdenzel <phdenzel@gmail.com>
2026-04-06 08:39:59 +0200

    feat: change color scheme for mealie

--- hosts/_srv/mealie.nix
+++ hosts/_srv/mealie.nix
@@ -9,6 +9,21 @@
       DEFAULT_GROUP = "Home";
       DEFAULT_HOUSEHOLD = "Family";
       TZ = "Europe/Zurich";
+      # Theme colors
+      THEME_LIGHT_PRIMARY = "#6666cc";
+      THEME_LIGHT_ACCENT = "#3366cc";
+      THEME_LIGHT_SECONDARY = "#3366cc";
+      THEME_LIGHT_SUCCESS = "#00AF87";
+      THEME_LIGHT_INFO = "#4DB5BD";
+      THEME_LIGHT_WARNING = "#FDB760";
+      THEME_LIGHT_ERROR = "#D7005F";
+      THEME_DARK_PRIMARY = "#6666cc";
+      THEME_DARK_ACCENT = "#3366cc";
+      THEME_DARK_SECONDARY = "#3366cc";
+      THEME_DARK_SUCCESS = "#00AF87";
+      THEME_DARK_INFO = "#4DB5BD";
+      THEME_DARK_WARNING = "#FDB760";
+      THEME_DARK_ERROR = "#D7005F";
     };
   };
 
