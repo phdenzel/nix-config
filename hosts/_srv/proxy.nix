7ce6ff4a575d1d3ba6b4dbda6399cace16b775fe
phdenzel <phdenzel@gmail.com>
2026-04-13 00:07:58 +0200

    feat: traefik service for ygdrasil

---
+++ hosts/_srv/proxy.nix
@@ -0,0 +1,81 @@
+{config, ...}: let
+  hosts = {
+    ygdrasil = "127.0.0.1";
+    heimdall = "192.168.178.64";
+    phinix = "192.168.178.156";
+  };
+  mkGlancesRouter = name: {
+    rule = "Host(`glances.${name}.home`)";
+    service = "glances-${name}";
+    entryPoints = ["web"];
+  };
+  mkGlancesService = ip: {
+    loadBalancer.servers = [{url = "http://${ip}:${toString config.services.glances.port}";}];
+  };
+in {
+  services.traefik = {
+    enable = true;
+
+    staticConfigOptions = {
+      entryPoints.web.address = ":80";
+      api = {
+        dashboard = true; # traefik dashboard at http://ygdrasil.home:8080
+        insecure = true; # acceptable for LAN-only
+      };
+    };
+
+    dynamicConfigOptions = {
+      http = {
+        routers = {
+          dashboard = {
+            rule = "Host(`denzels.home`)";
+            service = "homepage-dashboard";
+            entryPoints = ["web"];
+          };
+          forgejo = {
+            rule = "Host(`forgejo.home`)";
+            service = "forgejo";
+            entryPoints = ["web"];
+          };
+          jellyfin = {
+            rule = "Host(`jellyfin.home`)";
+            service = "jellyfin";
+            entryPoints = ["web"];
+          };
+          transmission = {
+            rule = "Host(`transmission.home`)";
+            service = "transmission";
+            entryPoints = ["web"];
+          };
+          vikunja = {
+            rule = "Host(`vikunja.home`)";
+            service = "vikunja";
+            entryPoints = ["web"];
+          };
+          mealie = {
+            rule = "Host(`mealie.home`)";
+            service = "mealie";
+            entryPoints = ["web"];
+          };
+          glances-ygdrasil = mkGlancesRouter "ygdrasil";
+          glances-heimdall = mkGlancesRouter "heimdall";
+          glances-phinix   = mkGlancesRouter "phinix";
+        };
+
+        services = {
+          dashboard.loadBalancer.servers = [{url = "http://127.0.0.1:${toString config.services.homepage-dashboard.listenPort}";}];
+          forgejo.loadBalancer.servers = [{url = "http://127.0.0.1:${toString config.services.forgejo.settings.server.HTTP_PORT}";}];
+          jellyfin.loadBalancer.servers = [{url = "http://127.0.0.1:8096";}];
+          transmission.loadBalancer.servers = [{url = "http://127.0.0.1:${toString config.services.transmission.settings.rpc-port}";}];
+          vikunja.loadBalancer.servers = [{url = "http://127.0.0.1:${toString config.services.vikunja.port}";}];
+          mealie.loadBalancer.servers = [{url = "http://127.0.0.1:${toString config.services.mealie.port}";}];
+          glances-ygdrasil = mkGlancesService hosts.ygdrasil;
+          glances-heimdall = mkGlancesService hosts.heimdall;
+          glances-phinix   = mkGlancesService hosts.phinix;
+        };
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [80 8080]; # 8080 = Traefik dashboard
+}
--- hosts/ygdrasil/configuration.nix
+++ hosts/ygdrasil/configuration.nix
@@ -22,7 +22,7 @@ in {
     # ../_srv/ollama.nix # local LLM services
     ../_srv/dashboards.nix # server dashboards (homepage-dashboard, glances, ...)
     # ../_srv/admin.nix # monitoring (cockpit, uptime-kuma, gotify, ...)
-    # ../_srv/proxy.nix # server proxy services (traefik, crowdsec, keycloak, ...)
+    ../_srv/proxy.nix # server proxy services (traefik, crowdsec, keycloak, ...)
     # ../_srv/vpn.nix # VPN services (wireguard, tailscale, ...)
     ../_srv/forgejo.nix # git forge service (forgejo)
     # ../_srv/cloud.nix # cloud service (filebrowser, opencloud, ...)
