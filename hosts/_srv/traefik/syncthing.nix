efed16e930a22988806f33e26d27d96fee449816
phdenzel <phdenzel@gmail.com>
2026-04-13 11:54:36 +0200

    fix: syncthing host fix using traefik middleware

--- hosts/_srv/traefik/syncthing.nix
+++ hosts/_srv/traefik/syncthing.nix
@@ -4,6 +4,10 @@
       rule = "Host(`syncthing.${config.networking.hostName}.home`)";
       service = "syncthing";
       entryPoints = ["web"];
+      middlewares = ["syncthing-hostfix"];
+    };
+    middlewares.syncthing-hostfix.headers.customRequestHeaders = {
+      Host = "localhost";
     };
     services.syncthing.loadBalancer.servers = [
       {
